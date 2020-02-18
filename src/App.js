dd9bae21608443155f1a9f713e62e66eeb9789c1
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2020-02-18 12:34:17 +0000

    Updated error handling for when invalid searches are made. Also deploying to Git pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function Gotd(props) {
-  console.log(props.result)
+  console.log(props.result);
   let final = "";
   let value = "";
 
@@ -10,98 +10,107 @@ function Gotd(props) {
     value = props.result[0].images.original.url;
     final = (
       <div className="container gotd">
-    <h2>Gif of the day!</h2> 
-    <img src={value} alt="gif of the day" />
-    </div> );
+        <h2>Gif of the day!</h2>
+        <img src={value} alt="gif of the day" />
+      </div>
+    );
   }
 
-  return (
-      final
-
-  )
+  return final;
 }
 
 class InputQuery extends React.Component {
-
   render() {
     return (
       <div>
-        <input onChange={this.props.handleInput} type="text" placeholder="Enter a search term"></input>
+        <input
+          onChange={this.props.handleInput}
+          type="text"
+          placeholder="Enter a search term"
+        ></input>
         <button onClick={this.props.handleSearch}>Search Giphy!</button>
         <button onClick={this.props.handleRemove}>Remove Images</button>
       </div>
-    )
+    );
   }
 }
 
 class Results extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
-    console.log(this.props.results)
+    console.log(this.props.results);
     let images = "";
     if (this.props.results.length > 0) {
-      images = this.props.results.map((val) => <img alt="" key={val.id} src={val.images.original.url} />);
+      images = this.props.results.map(val => (
+        <img alt="" key={val.id} src={val.images.original.url} />
+      ));
     }
 
-    return (
-      <div className="images" >
-        {images}
-      </div>
-    )
+    return <div className="images">{images}</div>;
   }
 }
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       inputValue: "",
       ajaxResponse: [],
-      gotd: [],
-    }
+      gotd: []
+    };
   }
 
   componentDidMount() {
-    this.search("gif of the day")
+    this.search("gif of the day");
   }
 
   search(day) {
-    //console.dir(e.target);
-    let test = day ? this.state.gotd : this.state.ajaxResponse;
+    let results = day ? this.state.gotd : this.state.ajaxResponse;
     let query = day || this.state.inputValue;
+    const Na = <h3>Nothing was found</h3>;
+    fetch(
+      "http://api.giphy.com/v1/gifs/search?q=" +
+        query +
+        "&api_key=dc6zaTOxFJmzC"
+    )
+      .then(result => result.json())
+      .then(res => {
+        if (res.data.length > 0) {
+          let random = Math.floor(Math.random() * res.data.length);
+          console.log(this, res, results, random);
+          //select a random image from the response and push that into the array
+          results.push(res.data[random]);
+        } else {
+          alert("Nothing was found. Try another Search term");
+          return;
+        }
 
-    fetch("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC")
-      .then((result) => result.json())
-      .then(function (res) {
-        let random = Math.floor(Math.random() * res.data.length);
-        return test.push(res.data[random])
-      })
-      .then(day ? () => this.setState({ gotd: test }) : () => this.setState({ ajaxResponse: test }))
-      .then(console.log(this.state))
-
+        if (day) {
+          this.setState({ gotd: results });
+        } else if (results.length > 0) {
+          this.setState({ ajaxResponse: results });
+        } else {
+          this.setState({ ajaxResponse: Na });
+        }
+      });
   }
 
   remove(e) {
-    this.setState({ ajaxResponse: [] })
+    this.setState({ ajaxResponse: [] });
   }
 
   inputValue(e) {
-
-    this.setState({ inputValue: e.target.value })
+    this.setState({ inputValue: e.target.value });
   }
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <div className="container">
         <h1>GIPHY PARTY</h1>
         <InputQuery
-          handleInput={(e) => this.inputValue(e)}
-          handleSearch={(e) => this.search()}
-          handleRemove={(e) => this.remove(e)}
+          handleInput={e => this.inputValue(e)}
+          handleSearch={e => this.search()}
+          handleRemove={e => this.remove(e)}
         />
         <Gotd result={this.state.gotd} />
         <Results results={this.state.ajaxResponse} />
