d73d5599ad5e778b2844166d087ca4c987ba3503
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2020-02-22 14:03:29 +0000

    Updates

---
+++ precache-manifest.06c22cab2d609d9b8e8fdf526939f838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aac133f8362faabf3bcb607235a2189",
+    "url": "/giphyapi/index.html"
+  },
+  {
+    "revision": "12c179314a4633961453",
+    "url": "/giphyapi/static/css/main.9418167a.chunk.css"
+  },
+  {
+    "revision": "87645bddd37f8f479440",
+    "url": "/giphyapi/static/js/2.1b596449.chunk.js"
+  },
+  {
+    "revision": "12c179314a4633961453",
+    "url": "/giphyapi/static/js/main.d36397db.chunk.js"
+  },
+  {
+    "revision": "e62243717773a55c7885",
+    "url": "/giphyapi/static/js/runtime~main.8ca91cb8.js"
+  },
+  {
+    "revision": "0ad9567b261534f2105c5f821d91250d",
+    "url": "/giphyapi/static/media/Brown_Frame_l.0ad9567b.png"
+  }
+]);
