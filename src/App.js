22b0229810f53958771423ea85699c7938838355
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2020-02-25 05:43:18 +0000

    pushing to continue on other computer

--- src/App.js
+++ src/App.js
@@ -1,64 +1,9 @@
 import React, { Fragment } from "react";
 import "./App.css";
-import landscape from "./images/Brown_Frame_l.png";
-
-function GIFSearchInput(props) {
-  return (
-    <div>
-      <input
-        onChange={props.handleInput}
-        type="text"
-        placeholder="Enter a search term"
-        onKeyPress={e => {
-          e.key === "Enter" && props.handleSearch(e);
-        }}
-      ></input>
-      <button onClick={props.handleSearch}>Search Giphy!</button>
-      <button onClick={props.handleRemove}>Remove Images</button>
-    </div>
-  );
-}
-
-function GifOfTheDay(props) {
-  const resultFromApi = props.result[0];
-  if (resultFromApi !== undefined) {
-    const gifOfTheDayAPIURL = props.result[0].images.original.url;
-    return (
-      <div className="container gotd">
-        <h2>Gif of the day!</h2>
-        <img
-          style={{
-            borderImage: `url(${landscape}) 30`,
-            borderImageRepeat: "stretch"
-          }}
-          src={gifOfTheDayAPIURL}
-          alt="gif of the day"
-        />
-      </div>
-    );
-  } else return null;
-}
-
-function Results(props) {
-  if (props.results.length > 0) {
-    const images = props.results.map((val, index) => {
-      return (
-        <div className="image_frame">
-          <img
-            alt=""
-            key={val.id + index}
-            style={{
-              borderImage: `url(${landscape}) 30`,
-              borderImageRepeat: "stretch"
-            }}
-            src={val.images.fixed_width.url}
-          />
-        </div>
-      );
-    });
-    return <div className="images">{images}</div>;
-  } else return null;
-}
+import GIFSearchInput from "./components/GIFSearchInput";
+import GifOfTheDay from "./components/GifOfTheDay";
+import GIFResults from "./components/GIFResults";
+import searchForInputValue from "./components/searchForInputValue";
 
 class App extends React.Component {
   constructor(props) {
@@ -68,36 +13,14 @@ class App extends React.Component {
       ajaxResponse: [],
       gotd: []
     };
+    searchForInputValue = searchForInputValue.bind(...arguments, this);
   }
 
   componentDidMount() {
-    this.searchForInputValue("gif of the day", "gotd");
+    searchForInputValue("gif of the day", "gotd");
   }
 
-  searchForInputValue(valueToSearch, arrayForResult) {
-    fetch(
-      "https://api.giphy.com/v1/gifs/search?q=" +
-        valueToSearch +
-        "&api_key=dc6zaTOxFJmzC"
-    )
-      .then(resultFromAPIBuffer => resultFromAPIBuffer.json())
-      .then(resultFromAPI_JSON => {
-        if (resultFromAPI_JSON.data.length > 0) {
-          const randomNumber = Math.floor(
-            Math.random() * resultFromAPI_JSON.data.length
-          );
-          //select a random image from the response and push that into the array
-          const selectedGIF = resultFromAPI_JSON.data[randomNumber];
-          const arrayWithSelectedGif = this.state[arrayForResult].push(
-            selectedGIF
-          );
-          this.setState({ arrayForResult: arrayWithSelectedGif });
-        } else {
-          alert("Nothing was found. Try another Search term");
-          return;
-        }
-      });
-  }
+  //searchForInputValue = (valueToSearch, arrayForResult)=> searchForInputValue(valueToSearch, arrayForResult);
 
   removeAllGifs(e) {
     this.setState({ ajaxResponse: [] });
@@ -115,14 +38,14 @@ class App extends React.Component {
           <GIFSearchInput
             handleInput={e => this.inputValue(e)}
             handleSearch={e =>
-              this.searchForInputValue(e.target.value, "ajaxResponse")
+              searchForInputValue(e.target.value, "ajaxResponse")
             }
             handleRemove={e => this.removeAllGifs(e)}
           />
         </header>
         <main className="container">
           <GifOfTheDay result={this.state.gotd} />
-          <Results results={this.state.ajaxResponse} />
+          <GIFResults results={this.state.ajaxResponse} />
         </main>
       </Fragment>
     );
